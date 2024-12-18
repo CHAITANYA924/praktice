c22db1aef16f6d7091d64962478ce0a2af5be2d3
CHAITANYA924 <koolchaitanya.143@gmail.com>
2024-12-18 20:41:03 +0530

    data

--- node.js
+++ node.js
@@ -1,10 +1,21 @@
-let data ;
-let promise = new Promise((resolve,reject)=>{
-if(data===true){
-  let ans = "i love my india";
-  resolve(ans)
-}else{
-  let failed = "he is not";
-  reject(failed)
+let fetchQuote=async()=>{
+  let data = await fetch("https://api.github.com/users");
+  let returnData =await data.json();
+  console.log(returnData);
+  onConcole(returnData)
+
 }
-}).then((item)=>console.log(item)).catch((err)=>console.log(err))
+fetchQuote()
+let onConcole=(details)=>{
+  details.forEach((item)=>{
+    let head = document.querySelector(".head");
+    console.log(item.login)
+
+    let title =document.createElement("h1")
+    title.textContent=item.login;
+    head.append(title)
+   
+  })
+}
+
+
